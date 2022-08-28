64731cd5b596334b9fc77cde4378d633257bff71
Lewis Kim <lewiskim517@gmail.com>
2022-08-28 12:46:53 -0400

    Updated game with DOM Manipulation

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
+const startButton = document.querySelector("#startButton");
+const selectionButtons = document.querySelectorAll("#selections input")
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const selections = document.querySelector("#selections");
+const scoreCount = document.querySelector(".scoreCount");
+const roundResult = document.querySelector(".roundResult");
+
+let count = 0;
+let round = 0;
 function computerPlay() {
     let choices = ["Rock", "Paper", "Scissors"];
-    return choices[Math.floor(Math.random() * 2)];
+    return choices[Math.floor(Math.random() * 3)];
 }
 
-function playerSelection() {
-    let playerChoice = prompt("Please enter Rock, Paper, or Scissors: ");
-    return playerChoice;
-}
 
-function playRound() {
+function playRound(selection) {
     let computerChoice = computerPlay().toLowerCase();
-    let playerChoice = playerSelection().toLowerCase();
+    let playerChoice = selection.toLowerCase();
 
-    let container = document.getElementById('gameContainer');
-
+   
     if (computerChoice == playerChoice) {
-        container.insertAdjacentHTML('beforeend', "<p>Tie! Computer selected " + computerChoice + " and you selected " + playerChoice + "</p>");
+        round++;
+        roundResult.textContent = "Round: " + round + ": Tie! Computer selected " + computerChoice + " and you selected " + playerChoice;
     } else if ((computerChoice == "rock" && playerChoice == "scissors") || (computerChoice == "paper" && playerChoice == "rock") || (computerChoice == "scissors" && playerChoice == "paper") ) {
-        container.insertAdjacentHTML('beforeend', "<p>You lost! Computer selected " + computerChoice + " and you selected " + playerChoice + "</p>");
-
+        round++;
+        roundResult.textContent = "Round: " + round + ": You lost! Computer selected " + computerChoice + " and you selected " + playerChoice ;
     } else if ((computerChoice == "scissors" && playerChoice == "rock") || (computerChoice == "rock" && playerChoice == "paper") || (computerChoice == "paper" && playerChoice == "scissors") ) {
-        container.insertAdjacentHTML('beforeend', "<p>You won! Computer selected " + computerChoice + " and you selected " + playerChoice + "</p>");
+        round++;
+        roundResult.textContent =  "Round: " + round + ": You won! Computer selected " + computerChoice + " and you selected " + playerChoice ;
+        count++;
+        scoreCount.textContent = "Score Count: " + count;
     } else {
-        container.insertAdjacentHTML('beforeend', "<p>Please select one of the options." + "</p>");
+        roundResult.textContent =  "Please select one of the options.";
     }
+
+    if (count == "5") {
+        alert("Congrats you win!");
+        startButton.style.display = "block";
+        selections.style = "none";
+        count = 0;
+        round = 0;
+        scoreCount.textContent = "";
+        roundResult.textContent = "";
+    } 
 }
 
-function game() {
-        playRound();
-}
+startButton.addEventListener("click", (e) => {
+    e.target.style.display = "none";
+    selections.style = "display: flex; flex-direction: column;";
+});
+
+selectionButtons.forEach((buttons) => {
+    buttons.addEventListener("click", (e) => {
+        playRound(e.target.attributes.value.value);
+    });
+});
